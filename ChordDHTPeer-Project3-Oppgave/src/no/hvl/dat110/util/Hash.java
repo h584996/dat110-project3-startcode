72043211f11a5cb61119a599417a57ca9de22a51
ikg <584996@stud.hvl.no>
2021-04-09 13:39:20 +0200

    Implemented the first method in Hash.java

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -30,6 +30,36 @@ public class Hash {
 		
 		// return the BigInteger
 		
+
+        // Static getInstance method is called with hashing MD5
+        MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			
+
+	        // digest() method is called to calculate message digest
+	        //  of an input digest() return array of byte
+	        byte[] messageDigest = md.digest(entity.getBytes());
+
+	        // Convert byte array into signum representation
+	        BigInteger no = new BigInteger(1, messageDigest);
+
+	        // Convert message digest into hex value
+	        String hashtext = no.toString(16);
+	        while (hashtext.length() < 32) {
+	            hashtext = "0" + hashtext;
+	        }
+	        
+	        //Convert hex into binInteger
+	        
+	        hashint = new BigInteger(hashtext,16);
+	        
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+
+		
 		return hashint;
 	}
 	
